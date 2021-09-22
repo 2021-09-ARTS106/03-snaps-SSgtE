4ced79ecb5e9a3929934d96de91115753e496dce
Patrick Esmerado <89804840+SSgtE@users.noreply.github.com>
2021-09-21 21:10:18 -0400

    Finished

---
+++ hdrrenderpipeline.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class hdrrenderpipeline : MonoBehaviour
+{
+
+
+    public hdrrenderpipeline exampleAssetA;
+    public hdrrenderpipeline exampleAssetb;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GraphicsSettings.renderPipleineAsset = exampleAssetA;
+            Debug.Log("Defaultrender pipeline asset is: " + GraphicsSettings.renderPipelineAsset.name);
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            GraphicsSettings.renderPipleineAsset = exampleAssetB;
+            Debug.Log("Defaultrender pipeline asset is: " + GraphicsSettings.renderPipelineAsset.name);
+        }
+    }
+
+}
